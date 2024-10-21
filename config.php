218136fc426dcd0b78f2251bd263dd1750b7a595
Ven0mSkull <167183884+Ven0mSkull@users.noreply.github.com>
2024-10-21 16:42:03 +0100

    Primeiro Commit

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+    
+    $dbHost = 'LocalHost';
+    $dbUsername = 'root';
+    $dbPassword = '';
+    $dbName = 'formulario_idty';
+    
+    $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+
+   // if($conexao -> connect_errno)
+  //   {
+   //      echo "Error";
+  //   }
+    // else
+   //  {
+  //       echo "Hackeado com sucesso";
+  //   }
+?>
